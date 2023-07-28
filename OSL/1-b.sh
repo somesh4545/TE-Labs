cfc3289298540aa07b9eab63f07f405a69a7bf6d
Somesh Somani <57597556+somesh4545@users.noreply.github.com>
2023-07-28 06:55:21 +0000

    OSL

--- OSL/1-b.sh
+++ OSL/1-b.sh
@@ -11,22 +11,26 @@ create () {
 
 view () {
     i=1
+    flag=0
     while read line
     do
+        flag=1
         echo "ID: $i Data: $line"
         i=$((i+1))
     done < $FILE
+
+    if [ $flag -eq 0 ]
+    then
+        echo "No records found"
+    fi
 }
 
 insert () {
-    echo "Enter name: "
-    read name
-    echo "Enter phone number: "
-    read phone_no
+    read -p "Enter name: " name
+    read -p "Enter phone number: " phone_no
     echo "$name - $phone_no" >> $FILE
 
-    echo "Add more records or exit: (y/n)"
-    read op
+    read -p "Add more records or exit: (y/n)" op
     if [ "$op" = "y" ]
     then
         insert
@@ -34,26 +38,33 @@ insert () {
 }
 
 delete () {
-    echo "Enter the name to be deleted: "
-    read name
-
-    while read line
-    do
-        IFS='-'
-        read -a arr <<< "$line"
-        echo "${arr[0]}"
-        # if [[ $line =~ "$name" ]]
-        # then
-        #     echo "Found details"
-        # fi
-    done < $FILE
+    read -p "Enter the name to be deleted: " name
+    result=`sed -i "/$name - */d" $FILE`
 }
 
-while [ $OP -ne 06 ]
+find () {
+    read -p "Enter the name: " name
+    result=`grep -i "$name" "$FILE"`
+    if test -z "$result"
+    then
+        echo "Not found"
+    else
+        echo "Found record - $result"
+    fi
+}
+
+modify () {
+    read -p "Enter the name to update: " name
+    read -p "Enter updated phone number: " number
+    result=`sed -i "/$name - */d" $FILE`
+    echo "$name - $number" >> $FILE
+}
+
+while [ $OP -ne 07 ]
 do
 
-    echo "\n1.Create 2.View 3.Insert 4.Delete 5.Modify 6. Exit"
-    read OP
+    echo "\n1.Create 2.View 3.Insert 4.Delete 5.Modify 6. Find 7. Exit"
+    read -p "Enter choice: " OP
 
     case $OP in
     1) create
@@ -64,7 +75,11 @@ do
     ;;
     4) delete
     ;;
-    6) echo "Exit"
+    5) modify
+    ;;
+    6) find
+    ;;
+    7) echo "Exit"
     ;;
     *) echo "default"
     ;;
