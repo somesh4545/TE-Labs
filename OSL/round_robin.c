1534ae555549b43060e734f832c0557519f2856e
Somesh Somani <57597556+somesh4545@users.noreply.github.com>
2023-08-28 20:11:16 +0000

    OSL

--- OSL/round_robin.c
+++ OSL/round_robin.c
@@ -2,13 +2,17 @@
 #include <stdlib.h>
 #define MAX 10
 
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
 int cmpbyarrivaltime( const void *pa, const void *pb ) {
-    const int (*a)[6] = pa;
-    const int (*b)[6] = pb;
-    if ( (*a)[2] < (*b)[2] ) return -1;
-    if ( (*a)[2] > (*b)[2] ) return +1;
+    const int (*a)[7] = pa;
+    const int (*b)[7] = pb;
+    if ( (*a)[1] < (*b)[1] ) return -1;
+    if ( (*a)[1] > (*b)[1] ) return +1;
     return 0;
 }
+
+// [0] id [1] AT [2] BT [3] CT [4] TAT [5] WT [6] remaining time
 
 void os_rr(int process[MAX][7], int n, int col, int time_q){
     int i=0, j=0;
@@ -17,10 +21,32 @@ void os_rr(int process[MAX][7], int n, int col, int time_q){
     int total_time = 0;
     for(i=0; i<n; i++) total_time += process[i][2];
     
-    printf("\n\nPID\tAT\tBt\tCT\tTaT\tWT\tRT\n");
-    int t_tat=0, t_wt=0;
-    for(i=0; i<n;){
+    printf("\n\nPID\tAT\tBt\tCT\tTaT\tWT\n");
+    int count = 0;
+    int t_tat=0, t_wt=0, index=0;
+    for(i=0; i<total_time;){
+        while(process[index][6]==0) index  = (index+1)%n;
         
+        int val = time_q < process[index][6] ? time_q : process[index][6];
+        i+= val;
+        process[index][6] -= val;
+
+        if(process[index][6] == 0){
+            count++;
+            process[index][3] = i;
+            process[index][4] = i-process[index][1];
+            t_tat+=process[index][4];
+            process[index][5] = process[index][4] - process[index][2];
+            t_wt += process[index][5];
+        }
+        if(count==n) break;
+        index  = (index+1)%n;
+    }
+
+    for(i=0; i<n; i++){
+        for(j=0; j<6; j++)
+            printf("%d\t",process[i][j]); 
+        printf("\n");
     }
     
     float avg_tat = (float)t_tat/n;
@@ -53,6 +79,7 @@ int main ( ) {
     printf("Enter the burst time: ");
     for(i=0; i<n; i++){
         scanf("%d", &process[i][2]);
+        process[i][6] = process[i][2];
     }
     qsort(process, n, sizeof(process[0]), cmpbyarrivaltime);
     os_rr(process, n, 6, time_q);
